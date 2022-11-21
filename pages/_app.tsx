a001d942aaafb217fa87f8b8f9a961443b2555d1
Eric McGary <mcgaryes@gmail.com>
2022-11-21 09:34:37 -0500

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import Image from "next/image";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <div>
+
+            <Component {...pageProps} />
+
+            <footer className={"footer"}>
+
+                <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                   target="_blank"
+                   rel="noopener noreferrer">
+                    Powered by{' '}
+                    <span className={"logo"}>
+                        <Image src="/vercel.svg"
+                               alt="Vercel Logo"
+                               width={72}
+                               height={16}/>
+                    </span>
+                </a>
+            </footer>
+
+        </div>
+    )
 }
