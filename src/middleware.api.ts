dfe83fa314b01123efd290172ca7f72fe7b6067d
Eric McGary <mcgaryes@gmail.com>
2022-12-15 17:48:53 -0500

    add auth api endpoint

---
+++ src/middleware.api.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from 'next/server'
+import {logger} from "@/utilities/logger";
+
+export const config = {
+    matcher: ['/((?!api|_next/static|favicon.ico).*)'],
+}
+
+export function middleware(req: NextRequest) {
+
+    const basicAuth = req.headers.get('authorization')
+    const url = req.nextUrl
+
+    if (basicAuth) {
+        const authValue = basicAuth.split(' ')[1]
+        const [user, pwd] = atob(authValue).split(':')
+
+        if (user === process.env.BASIC_AUTH_USERNAME && pwd === process.env.BASIC_AUTH_PASSWORD) {
+            return NextResponse.next()
+        }
+    }
+
+    url.pathname = '/api/auth'
+
+    return NextResponse.rewrite(url)
+
+}
